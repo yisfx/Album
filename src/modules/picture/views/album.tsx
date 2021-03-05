82e64b8a00a473be51e743ac4653f669614ebcc9
fx-wangsai <wangsaihaizai@outlook.com>
2021-03-05 21:51:54 +0800

    home page

--- src/modules/picture/views/album.tsx
+++ src/modules/picture/views/album.tsx
@@ -5,22 +5,24 @@ import Swiper, { ReactIdSwiperProps } from "react-id-swiper";
 
 if (process.env.BROWSER) {
     require('../../../../static/css/main.css')
-    require('swiper/dist/css/swiper.mini.css')
+    require('swiper/dist/css/swiper.css')
 }
 
 
 function Cover() {
-
+    const pc = window.screen.height < window.screen.width
     const mmm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,]
     const params = {
         direction: 'horizontal',
-        slidesPerView : 1, //显示数量
-        grabCursor: true,
+        // grabCursor: true,
         setWrapperSize: true,//开启这个设定会在Wrapper上添加等于slides相加的宽高，在对flexbox布局的支持不是很好的浏览器中可能需要用到
         roundLengths: true,//设定为true将slide的宽和高取整(四舍五入)以防止某些屏幕上文字模糊
-        loop: false,
-        observer: true,
-        observeSlideChildren: true,
+        // loop: false,
+        // observer: true,
+        // observeSlideChildren: true,
+        pagination: {
+            el: '.swiper-pagination'
+        },
         on: {
             paginationRender: () => {
                 console.log("paginationRender")
@@ -30,9 +32,13 @@ function Cover() {
     return <>
         <Swiper {...params}>
             {mmm.map((v, i) => i % 2 == 0 ?
-                <div key={i} className="side-container">{i}</div>
+                <div key={i} className="main-sider" style={{ height: "100vh" }}>
+                    <img className={pc?"main-img-pc":"main-img-mobile"} src={"http://localhost:9000/kjsdfh/album/aaa-IMG_20210217_193800-max.jpg"} />
+                </div>
                 :
-                <div key={i} className="side-container">{i}</div>
+                <div key={i} className="main-sider" style={{ height: "100vh" }}>
+                    <img className={pc?"main-img-pc":"main-img-mobile"} src={"http://localhost:9000/kjsdfh/album/aaa-IMG_20210217_193742-max.jpg"} />
+                </div>
 
             )}
         </Swiper>
@@ -45,12 +51,9 @@ function Content() {
             <div className="main-menum">
                 <button className="btn">---___</button>
             </div>
-            <div style={{ padding: "5px", height: "100%" }}>
-                <div className="Cover">
-                    <Cover />
-                </div>
+            <div className="Cover">
+                <Cover />
             </div>
-
         </div>
     )
 }
