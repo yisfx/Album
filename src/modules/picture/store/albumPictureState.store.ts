917447d3d8d7002be093eed1dc4bdfccf3545abf
fx-wangsai <wangsaihaizai@outlook.com>
2021-03-17 21:52:07 +0800

    list

--- src/modules/picture/store/albumPictureState.store.ts
+++ src/modules/picture/store/albumPictureState.store.ts
@@ -1,5 +1,26 @@
+import React from "react";
 import { Album } from "../../../model/album";
 
 export interface AlbumPictureState {
     Album: Album
+}
+
+export type AlbumPictureAction = { action: "init", }
+    | { action: "update", data: Partial<AlbumPictureState> }
+
+export type AlbumPictureContextType = {
+    state: AlbumPictureState,
+    dispatcher: React.Dispatch<AlbumPictureAction>
+}
+
+export const AlbumPictureContext = React.createContext<AlbumPictureContextType>(undefined);
+
+
+export function AlbumPictureReducer(state: AlbumPictureState, action: AlbumPictureAction) {
+    switch (action.action) {
+        case "init":
+            return { ...state }
+        case "update":
+            return { ...state, ...action.data }
+    }
 }
--- src/modules/picture/views/albumPicture.tsx
+++ src/modules/picture/views/albumPicture.tsx
@@ -1,8 +1,30 @@
-import React from "react"
+import React, { useContext } from "react"
 import MasterPage from "../../../framework/Master/@masterPage"
 import Master from "../../../framework/Master/master"
-import { AlbumPictureState } from "../store/AlbumPictureState.store"
+import { AlbumPictureContext, AlbumPictureReducer, AlbumPictureState } from "../store/AlbumPictureState.store"
 
+
+
+
+function Top() {
+    const { state, dispatcher } = useContext(AlbumPictureContext)
+    debugger
+    return <div className="page-header">
+        <h3>{state.Album.Name}
+            <br />
+            <small>{state.Album.Description}</small>
+        </h3>
+    </div>
+}
+
+function Content(initalState: AlbumPictureState) {
+
+    const [state, dispatcher] = React.useReducer(AlbumPictureReducer, initalState)
+
+    return <AlbumPictureContext.Provider value={{ state, dispatcher }}>
+        <Top />
+    </AlbumPictureContext.Provider>
+}
 
 
 @MasterPage(Master)
@@ -12,9 +34,7 @@ export class AlbumPicturePage extends React.Component<AlbumPictureState, any>{
     }
     render() {
         return <div className="container">
-            <div className="row">
-                
-            </div>
+            <Content {...this.props} />
         </div>
     }
 }
