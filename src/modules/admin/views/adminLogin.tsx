bdbb11cb04ea7b8b526ac3b5fb0a647c1a8b9712
fx-wangsai <wangsaihaizai@outlook.com>
2021-03-29 20:04:10 +0800

    login

--- src/modules/admin/views/adminLogin.tsx
+++ src/modules/admin/views/adminLogin.tsx
@@ -1,33 +1,64 @@
 import React, { useContext, useReducer, useState } from "react";
 import Master from "../../../framework/Master/master";
 import MasterPage from "../../../framework/Master/@masterPage";
+import { Ajax } from "src/framework/httpclient/ajax";
 
 if (process.env.BROWSER) {
     require('../../../../static/css/login.css')
 }
 
-function Content(props: any) {
+
+function Pwd(props: { K: string, setValue: (key: string, value: string) => void }) {
     const [showPwd, setShowPwd] = useState(false);
     const [pwd, setPwd] = useState("");
 
-    return <div className="container">
-
-        <div className="row">
-            <div className="col-xs-12">
-                <div style={{ height: "30px" }}></div>
-                <div className="input-group">
-                    <input type={showPwd ? "text" : "password"} className="form-control" value={pwd}
-                        onChange={(evt) => {
-                            setPwd(evt.target.value);
-                        }} />
-                    <div className="input-group-btn">
-                        <button className="btn btn-default" type="button">{showPwd ? "hide" : "Show"}</button>
-                        <button className="btn btn-default" type="button">Input</button>
-                    </div>
-                </div>
-            </div>
+    return <div className="input-group">
+        <div className="input-group-btn">
+            <button className="btn btn-default" type="button">{props.K}</button>
+        </div>
+        <input type={showPwd ? "text" : "password"} className="form-control" value={pwd}
+            onChange={(evt) => {
+                setPwd(evt.target.value);
+                props.setValue(props.K, pwd);
+            }} />
+        <div className="input-group-btn">
+            <button className="btn btn-default" type="button"
+                onClick={() => {
+                    setShowPwd(!showPwd);
+                }}
+            >{showPwd ? "hide" : "Show"}</button>
         </div>
     </div>
+}
+
+function Content(props: any) {
+    const initPwdList = { A: "", B: "", C: "", D: "" }
+    const pwdKeyList = ["A", "B", "C", "D"]
+    const [pwd, setPwd] = useState(initPwdList);
+
+    const submit = () => {
+        Ajax("loginapi", pwd).then(resp => {
+            if (resp.Result) {
+                ///set cookie
+                ///
+            }
+        })
+    }
+
+    return <div className="container">
+        < div className="row" >
+            <div className="col-xs-12">
+                <div style={{ height: "30px" }}></div>
+                {pwdKeyList.map(key => <Pwd key={key} K={key}
+                    setValue={(key, value) => {
+                        let v = pwd;
+                        v[key] = value
+                        setPwd({ ...v });
+                    }} />)}
+                <button type="button" className="btn btn-primary">（首选项）Primary</button>
+            </div>
+        </div >
+    </div >
 }
 
 @MasterPage(Master)
