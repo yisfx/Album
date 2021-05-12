07ccf211579bd33a9aab5f6e65ddbe9b7ea62fb3
yisfx <wangsaihaizai@outlook.com>
2021-05-12 10:59:57 +0800

    Update dockerRun.ts

--- dockerRun.ts
+++ dockerRun.ts
@@ -1,6 +1,3 @@
 import { exec } from "child_process";
 
-exec("docker run -v /c/users/wangs/desktop/album/dockerdesktop/global:/app/global album")
-
-
-docker run -v $PWDC:\users\wangs\desktop\album\dockerdesktop\global\:/app/global album
+exec("docker run -v /home/ubuntu/application/album/:/app/album album")
