074740368323c029aee8689ec73372a48f0a0248
fx-wangsai <wangsaihaizai@outlook.com>
2021-05-12 21:12:55 +0800

    path

--- src/conf/site.config.ts
+++ src/conf/site.config.ts
@@ -1,15 +1,13 @@
 import { SystemConfig } from "../model/SystemConfig"
 const SysConfig: SystemConfig = {
     port: 9000,
-
     domain: "http://localhost:9000",
     VisualStaticPath: "/kjsdfh",
     MixPath: "/mix",
     JsPath: "public/script",
     CssPath: "public/css",
     ImagePath: "public/image",
-
-    GlobalConfigPath: "C://Users//wangs//Desktop//album//DockerDesktop//global/global.setting.json",
+    GlobalConfigPath: "/home/ubuntu/application/album/global.setting.json",
 }
 
 
