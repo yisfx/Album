0e9b8310c446679cd42e6f97cb5ec814902fd61d
yisfx <wangsaihaizai@outlook.com>
2022-08-25 11:09:03 +0800

    备案信息与实际网站名不一致，shit
    生活与旅行的记录

--- src/framework/template/ReactView.ts
+++ src/framework/template/ReactView.ts
@@ -19,7 +19,7 @@ const RenderHtml = (data: any, cssFile: string[], scriptFile: string[], response
     const html = `<!DOCTYPE html>
         <html lang="zh-CN">
         <head>
-            <title>TRAVEL</title>
+            <title>生活与旅行的记录</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         </head>
         <body>
